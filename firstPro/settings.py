4163ac656bee97c2786b97bac3285978ab68a761
Milindcode <milindsingh1004@gmail.com>
2022-09-19 21:50:51 +0530

    deployment ready

--- firstPro/settings.py
+++ firstPro/settings.py
@@ -127,3 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "/login"
 LOGIN_REDIRECT_URL= '/home/'
 LOGOUT_REDIRECT_URL= '/login/'
+
+import django_heroku
+django_heroku.settings(locals())
